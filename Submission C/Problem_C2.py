37203a52f7b5149dcd53018fb51f628ad1e14fed
andiraraharjo <67132890+andiraraharjo@users.noreply.github.com>
2022-07-30 07:09:22 +0700

    Submission C

---
+++ Submission C/Problem_C2.py
@@ -0,0 +1,45 @@
+# =============================================================================
+# PROBLEM C2
+#
+# Create a classifier for the MNIST Handwritten digit dataset.
+# The test will expect it to classify 10 classes.
+#
+# Don't use lambda layers in your model.
+#
+# Desired accuracy AND validation_accuracy > 91%
+# =============================================================================
+
+
+import tensorflow as tf
+
+def solution_C2():
+    mnist = tf.keras.datasets.mnist
+    (train_images, train_labels), (test_images, test_labels) = mnist.load_data()
+
+    train_images = train_images / 255.0
+
+    test_images = test_images / 255.0
+
+    model = tf.keras.Sequential([
+        tf.keras.layers.Flatten(input_shape=(28, 28)),
+        tf.keras.layers.Dense(256, activation='relu'),
+        tf.keras.layers.Dense(10)
+    ])
+
+    model.compile(optimizer='adam',
+                  loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+                  metrics=['accuracy'])
+
+    model.fit(train_images, train_labels, validation_data=(test_images, test_labels), epochs=10)
+
+    # YOUR CODE HERE
+
+    return model
+
+
+# The code below is to save your model as a .h5 file.
+# It will be saved automatically in your Submission folder.
+if __name__ == '__main__':
+    if __name__ == '__main__':
+        model = solution_C2()
+        model.save("model_C2.h5")
---
+++ Submission C/Problem_C4.py
@@ -0,0 +1,80 @@
+# =====================================================================================================
+# PROBLEM C4 
+#
+# Build and train a classifier for the sarcasm dataset. 
+# The classifier should have a final layer with 1 neuron activated by sigmoid.
+# 
+# Do not use lambda layers in your model.
+# 
+# Dataset used in this problem is built by Rishabh Misra (https://rishabhmisra.github.io/publications).
+#
+# Desired accuracy and validation_accuracy > 75%
+# =======================================================================================================
+
+import json
+import tensorflow as tf
+import numpy as np
+import urllib
+from tensorflow.keras.preprocessing.text import Tokenizer
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+
+def solution_C4():
+    data_url = 'https://github.com/dicodingacademy/assets/raw/main/Simulation/machine_learning/sarcasm.json'
+    urllib.request.urlretrieve(data_url, 'sarcasm.json')
+
+    vocab_size = 1000
+    embedding_dim = 16
+    max_length = 120
+    trunc_type = 'post'
+    padding_type = 'post'
+    oov_tok = "<OOV>"
+    training_size = 20000
+
+    sentences = []
+    labels = []
+    # YOUR CODE HERE
+    datastore = []
+    with open("sarcasm.json", 'r') as f:
+        for line in f:
+            datastore.append(json.loads(line))
+
+    for item in datastore:
+        sentences.append(item['headline'])
+        labels.append(item['is_sarcastic'])
+
+    training_sentences = sentences[0:training_size]
+    valdiation_sentences = sentences[training_size:]
+    training_labels = labels[0:training_size]
+    validation_labels = labels[training_size:]
+
+    tokenizer = Tokenizer(num_words=vocab_size, oov_token=oov_tok)
+    tokenizer.fit_on_texts(training_sentences)
+
+    training_sequences = tokenizer.texts_to_sequences(training_sentences)
+    training_padded = np.array(pad_sequences(training_sequences, maxlen=max_length, padding=padding_type, truncating=trunc_type))
+
+    valditaion_sequences = tokenizer.texts_to_sequences(valdiation_sentences)
+    validation_padded = np.array(pad_sequences(valditaion_sequences, maxlen=max_length, padding=padding_type, truncating=trunc_type))
+
+    training_labels = np.array(training_labels)
+    validation_labels = np.array(validation_labels)
+
+    model = tf.keras.Sequential([
+        tf.keras.layers.Embedding(vocab_size, embedding_dim, input_length=max_length),
+        tf.keras.layers.GlobalAveragePooling1D(),
+        tf.keras.layers.Dense(24, activation='relu'),
+        tf.keras.layers.Dense(1, activation='sigmoid')
+    ])
+    model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+    model.fit(training_padded, training_labels, epochs=30, validation_data=(validation_padded, validation_labels),
+              verbose=2)
+
+    return model
+
+
+# The code below is to save your model as a .h5 file.
+# It will be saved automatically in your Submission folder.
+if __name__ == '__main__':
+    # DO NOT CHANGE THIS CODE
+    model = solution_C4()
+    model.save("model_C4.h5")
